966d982757bee9865407db98b3eb5bd9340bcf92
zhangyubo <yubozhang@umass.edu>
2023-08-14 01:51:29 -0400

    keep change shap path

--- num_gradient/num_gradient_descent_master/SHAP.py
+++ num_gradient/num_gradient_descent_master/SHAP.py
@@ -65,6 +65,8 @@ def calculate_f_scores_in_folder(folder_path, target_class=None,threshold = 0.5)
 #folder_path = "./shap_random_images"
 folder_path = os.path.join(os.path.dirname(__file__), "shap_random_images")
 
+generate_and_save_images(num_seeds,folder_path)
+
 
 selected_images = calculate_f_scores_in_folder(folder_path, target_class=classes.index(args.target),threshold = 0.5)
 

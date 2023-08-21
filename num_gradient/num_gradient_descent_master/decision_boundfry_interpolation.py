8721f892ac149c692634e714cf0321b5d64627f4
zhangyubo <yubozhang@umass.edu>
2023-08-21 17:48:29 -0400

    change the classes index names again

--- num_gradient/num_gradient_descent_master/decision_boundfry_interpolation.py
+++ num_gradient/num_gradient_descent_master/decision_boundfry_interpolation.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # 选择两个类别的代表性样本
 deer_sample = Image.open('./data/cifar_pictures/NO.15class4deer.jpg')  # 这里需要替换为实际的路径
-cat_sample = Image.open('./data/cifar_pictures/NO.0class3cat.jpg')  # 同样需要替换为实际的路径
+ship_sample = Image.open('./data/cifar_pictures/NO.2class8ship.jpg')  # 同样需要替换为实际的路径
 
 
 # 显示并分类代表性样本
@@ -17,16 +17,16 @@ def display_and_classify(sample, label):
     plt.show()
 
 display_and_classify(deer_sample, "deer")
-display_and_classify(cat_sample, "Cat")
+display_and_classify(ship_sample, "Cat")
 
 # 将这些样本转化为numpy数组
 deer_array = np.array(deer_sample)
-cat_array = np.array(cat_sample)
+ship_array = np.array(ship_sample)
 
 # 在这两个样本之间进行线性插值
 steps = 10  # 您可以更改此值以获取更多或更少的插值步骤
 alpha_values = np.linspace(0, 1, steps)
-interpolated_samples = [(1 - alpha) * deer_array + alpha * cat_array for alpha in alpha_values]
+interpolated_samples = [(1 - alpha) * deer_array + alpha * ship_array for alpha in alpha_values]
 
 # 将插值样本输入到模型中并观察输出
 for idx, sample in enumerate(interpolated_samples):

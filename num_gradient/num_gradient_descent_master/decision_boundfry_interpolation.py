1eceabc7227eca031aaf04b21e230c07cc966547
zhangyubo <yubozhang@umass.edu>
2023-08-21 21:49:46 -0400

    add the logic to generate the random images based on the decision boundary

--- num_gradient/num_gradient_descent_master/decision_boundfry_interpolation.py
+++ num_gradient/num_gradient_descent_master/decision_boundfry_interpolation.py
@@ -3,10 +3,22 @@ from PIL import Image
 from t_ngd_cifar10 import test_classifier, linearize_pixels
 import matplotlib.pyplot as plt
 
+
+
+def generate_random_noise(shape, intensity=0.05):
+    return np.random.uniform(-intensity, intensity, shape)
+
+def generate_random_image(base_image, alpha_shift, noise_intensity=0.05):
+    alpha = boundary_alpha + alpha_shift
+    interpolated_image = (1 - alpha) * frog_sample + alpha * ship_array
+    random_noise = generate_random_noise(interpolated_image.shape, noise_intensity)
+    random_image = interpolated_image + random_noise
+    return np.clip(random_image, 0, 255)
+
+
 # 选择两个类别的代表性样本
 frog_sample = Image.open('./data/cifar_pictures/NO.1class6frog.jpg')  # 这里需要替换为实际的路径
 ship_sample = Image.open('./data/cifar_pictures/NO.2class8ship.jpg')  # 同样需要替换为实际的路径
-
 
 # 显示并分类代表性样本
 def display_and_classify(sample, label):
@@ -28,6 +40,9 @@ steps = 25  # 您可以更改此值以获取更多或更少的插值步骤
 alpha_values = np.linspace(0, 1, steps)
 interpolated_samples = [(1 - alpha) * frog_sample + alpha * ship_array for alpha in alpha_values]
 
+boundary_alpha = None
+prev_class = None
+
 # 将插值样本输入到模型中并观察输出
 for idx, sample in enumerate(interpolated_samples):
     h, w, img_array = linearize_pixels(Image.fromarray(np.uint8(sample)))
@@ -37,3 +52,22 @@ for idx, sample in enumerate(interpolated_samples):
     plt.imshow(np.uint8(sample))
     plt.title(f"Alpha: {alpha_values[idx]}, Classified as: {identified_class}")
     plt.show()
+
+    if prev_class is not None and identified_class != prev_class:
+        boundary_alpha = alpha_values[idx]
+        break
+    prev_class = identified_class
+
+    if boundary_alpha:
+        # Generate random images on either side of the decision boundary
+        random_image_side_1 = generate_random_image(frog_sample, 0.05)
+        random_image_side_2 = generate_random_image(ship_sample, -0.05)
+
+        # Classify and display these random images
+        class_1 = display_and_classify(Image.fromarray(np.uint8(random_image_side_1)), "Random Image Side 1")
+        class_2 = display_and_classify(Image.fromarray(np.uint8(random_image_side_2)), "Random Image Side 2")
+
+        print(f"Random image from side 1 is classified as: {class_1}")
+        print(f"Random image from side 2 is classified as: {class_2}")
+    else:
+        print("No decision boundary found.")

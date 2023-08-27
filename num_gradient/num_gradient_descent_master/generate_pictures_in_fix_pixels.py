cc2dc00df68122f36652f51d70304df6ababfb55
zhangyubo <yubozhang@umass.edu>
2023-08-26 20:41:01 -0400

    show the fix pixel images

--- num_gradient/num_gradient_descent_master/generate_pictures_in_fix_pixels.py
+++ num_gradient/num_gradient_descent_master/generate_pictures_in_fix_pixels.py
@@ -1,13 +1,17 @@
 from PIL import Image
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 # 创建保存图片的文件夹，如果不存在的话
 if not os.path.exists('fix_pixels_images'):
     os.makedirs('fix_pixels_images')
 
+# 准备用于展示图片的 matplotlib 子图
+fig, axes = plt.subplots(1, 10, figsize=(20, 2))
+
 # 生成10张32*32的单色图片
-for pixel_value in range(127, 117, -1):
+for i, pixel_value in enumerate(range(127, 117, -1)):
     # 创建一个32*32*3的NumPy数组
     img_array = np.full((32, 32, 3), pixel_value, dtype=np.uint8)
 
@@ -15,6 +19,14 @@ for pixel_value in range(127, 117, -1):
     img = Image.fromarray(img_array, 'RGB')
 
     # 保存图片
-    img.save(f'fix_pixels_images/fix_pixels{pixel_value}.png')
+    img_path = f'fix_pixels_images/fix_pixels{pixel_value}.jpg'
+    img.save(img_path)
 
+    # 展示图片
+    axes[i].imshow(img)
+    axes[i].set_title(f'Pixel: {pixel_value}')
+    axes[i].axis('off')
+
+plt.show()
 print("图片生成和保存完成！")
+

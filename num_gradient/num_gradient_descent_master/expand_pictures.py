7defcb579aee2cc6c67fc530430a3cfeeeb396e3
zhangyubo <yubozhang@umass.edu>
2023-09-04 14:29:21 -0400

    add yhe preprocess

--- num_gradient/num_gradient_descent_master/expand_pictures.py
+++ num_gradient/num_gradient_descent_master/expand_pictures.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import numpy as np
 from t_ngd_cifar10 import test_classifier, linearize_pixels
-from t_ngd_cifar10 import net
+#from t_ngd_cifar10 import net
+from t_ngd_cifar10 import preprocess_image
 
 
 # 假设 test_classifier 函数已经定义，这里仅为示例
@@ -13,7 +14,7 @@ def generate_image_with_noise_and_classify(h, w, img_array, std_deviation):
     # 重新将一维数组转换为多维数组（图像）
     original_image = img_array.reshape((h, w, 3)).astype('uint8')
 
-    # 生成与原图相同形状的随机噪声（这里没有使用协方差矩阵）
+    # 生成与原图相同形状的随机噪声
     noise = np.random.normal(0, std_deviation, original_image.shape)
 
     # 将随机噪声添加到原图上
@@ -21,20 +22,21 @@ def generate_image_with_noise_and_classify(h, w, img_array, std_deviation):
 
     # 限制新像素值在合法范围内（例如，0到255对于uint8图像）
     new_image = np.clip(new_image, 0, 255).astype(np.uint8)
-    #net.eval()
+
+    # 预处理新图像
+    img_tensor = preprocess_image(h, w, new_image)
+
     # 保存新图像
     img_path = f"new_Image{std_deviation}.jpg"
     Image.fromarray(new_image, 'RGB').save(img_path)
     print(f"图像保存在 {img_path}")
 
     # 使用 test_classifier 函数进行分类
-    #net.eval()
-    img_array = new_image.reshape(h * w * 3).astype('float64')
-    predicted_class = test_classifier(h, w, img_array)
+    predicted_class = test_classifier(h, w, img_tensor)
     print(f"新图像的预测类别：{predicted_class}")
 
 # 使用 linearize_pixels 函数处理原始图像，并得到高度 h，宽度 w，和一维数组 img_array
-your_original_image = Image.open("./output.jpg")
+your_original_image = Image.open("./output.jpg")  # 这里仅为示例，请使用你自己的图片路径
 h, w, img_array = linearize_pixels(your_original_image)
 
 # 使用 generate_image_with_noise_and_classify 函数添加噪声并分类

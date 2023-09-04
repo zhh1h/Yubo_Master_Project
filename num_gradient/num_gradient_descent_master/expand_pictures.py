8937e3121e3ae5ee9d02cf2a6ab9210a471b2e98
zhangyubo <yubozhang@umass.edu>
2023-09-04 00:52:34 -0400

    change the method

--- num_gradient/num_gradient_descent_master/expand_pictures.py
+++ num_gradient/num_gradient_descent_master/expand_pictures.py
@@ -9,35 +9,37 @@ from t_ngd_cifar10 import test_classifier, linearize_pixels
 
 # 修改后的 generate_image_with_global_noise 函数，添加了保存图片和分类功能
 def generate_image_with_global_noise_and_classify(h, w, img_array, std_deviation):
-    # 重新将一维数组转换为多维数组（图像）
+    # 将1D数组重新整形为多维数组（图像）
     original_image = img_array.reshape((h, w, 3)).astype('uint8')
 
-    # 计算原图所有像素的全局平均值
-    global_mean = np.mean(original_image)
+    # 为简单起见，我们将使用单位矩阵作为协方差矩阵
+    identity_matrix = np.eye(h * w * 3)
 
-    # 生成与原图相同形状的随机噪声
-    noise = np.random.normal(global_mean, std_deviation, original_image.shape)
+    # 根据协方差矩阵生成随机噪声
+    noise = np.random.multivariate_normal(np.zeros(h * w * 3), identity_matrix * std_deviation ** 2)
+    noise = noise.reshape((h, w, 3))
 
-    # 将随机噪声添加到原图上
-    new_image = original_image + noise
+    # 将随机噪声添加到原图像上
+    new_image = original_image + noise.astype('uint8')
 
-    # 限制新像素值在合法范围内（例如，0到255对于uint8图像）
+    # 将新像素值限制在合法范围内（例如，对于uint8图像是0到255）
     new_image = np.clip(new_image, 0, 255).astype(np.uint8)
 
     # 保存新图像
     img_path = f"new_Image{std_deviation}.jpg"
     Image.fromarray(new_image, 'RGB').save(img_path)
-    print(f"Image saved at {img_path}")
+    print(f"图片已保存在 {img_path}")
 
-    # 使用 test_classifier 函数进行分类
+    # 使用test_classifier函数进行分类
     img_array = new_image.reshape(h * w * 3).astype('float64')
     predicted_class = test_classifier(h, w, img_array)
-    print(f"Predicted class for new image: {predicted_class}")
+    print(f"新图像的预测类别是：{predicted_class}")
 
-# 使用 linearize_pixels 函数处理原始图像，并得到高度 h，宽度 w，和一维数组 img_array
-your_original_image = Image.open("./output.jpg")
+
+# 使用linearize_pixels函数处理原始图像，得到高度h、宽度w和1D数组img_array
+your_original_image = Image.open("./output.jpg")  # 用您图像的路径替换
 h, w, img_array = linearize_pixels(your_original_image)
 
-# 使用 generate_image_with_global_noise_and_classify 函数添加噪声并分类
-std_deviation = 0
+# 使用generate_image_with_global_noise_and_classify函数添加噪声并分类
+std_deviation = 0  # 标准差（或“扩展”）可以根据需要进行调整
 generate_image_with_global_noise_and_classify(h, w, img_array, std_deviation)

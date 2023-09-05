5cd08d453b8b6e3598b8c56b129c4005ca70f29f
zhangyubo <yubozhang@umass.edu>
2023-09-05 01:55:17 -0400

    try to fix the coherence

--- num_gradient/num_gradient_descent_master/t_ngd_cifar10.py
+++ num_gradient/num_gradient_descent_master/t_ngd_cifar10.py
@@ -310,6 +310,7 @@ if args.input_pic:
                 print(f"gradient time: {time_interval} 秒")
             #if ((test_classifier(h, w, img_array)) == classes.index(args.target)) and (create_f(h, w, img_array) >= 0.5):
             #if create_f(h, w, classes.index(args.target)) >= 0.5:
+            test_classifier(h, w, img_array)
             final_time= time.time()
             final_interval = final_time - start_time0
             print(f"final time interval: {final_interval} 秒")
